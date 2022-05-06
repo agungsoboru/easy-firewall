e66d7fdf99c3da0b70993ba9d652d3154ab90896
gung <71138589+agungsoboru@users.noreply.github.com>
2022-05-06 15:52:25 +0800

    Update iptables-blok-ip.sh

--- iptables-blok-ip.sh
+++ iptables-blok-ip.sh
@@ -5,4 +5,5 @@ read -e -p "Enter Ban ip: " IP
 
 sudo iptables -I INPUT -s $IP -j DROP
 sudo iptables -I OUTPUT -s $IP -j DROP
+sudo iptables -A OUTPUT -d $IP -j DROP
 #sudo ufw reject from $IP to any
